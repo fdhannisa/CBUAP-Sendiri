9870acb8a9bc9609d88c5807471873858b17e91a
annisa <92286817+fdhannisa@users.noreply.github.com>
2021-11-26 14:03:45 +0700

    Update UAP.cpp

--- UAP.cpp
+++ UAP.cpp
@@ -1,23 +1,26 @@
 //progres2
 #include <iostream>
+#include <time.h>
+#include <stdlib.h>
 using namespace std;
 
 void garis(){for (int i = 0; i <= 30; i++){
 				 cout<< "=";}}
+
+void baris(){for (int i = 0; i <= 20; i++){
+				 cout<< "-";}}
 				 
 void kosong(){for (int i = 0; i <= 5; i++){
 				 cout<<endl;}}
 				 				 
 int main(){
 	string pilihan, input, deposit;
-	string Login = "nama";
+	string Login = "Mawar";
 	string tidak = "no";
 	string iya = "yes";
-	char nama,wallet,nomor,bet;
+	char nama,wallet,nomor;
 	bool yn;
-	int a = 20;
-	int b = 1000;
-	int y;
+	int y, bet;
 
 				 
 	garis();
@@ -29,21 +32,38 @@ int main(){
 	system("cls");
 
 	cout<< endl;
-	cout<< "      welcome to the game";
+	cout<< "      WELCOME TO THE GAME";
 	kosong();
 	cout<< "   	    Log In\n	    Sign In\n";
 	cin>> pilihan;
 
 	if (pilihan == Login){
-	cout<< "check my deposit money";
+	cout<< "Check my deposit money";
 	cin>> deposit;
 		if(deposit == tidak){
 		cout<< "How much money would you like to bet? $";
 		cin>> bet;
+		cout<< "Remining deposit: $"<< 1000 - bet<< endl;
 		}else {
-		cout<< "Your remaining deposit money is $1000";
-		}
-	}
+		cout<< "Your remaining deposit money is $1000\n";
+		cout<< "How much money would you like to bet? $";}
+		kosong();}
+	cout>> "Play\n";
+	getch();
+//progres3
+	cout<< "| Player 1 :"<< Login<< "\n";
+	baris();
+	cout<< "\n| Bet     :$"<< bet<< "\n";
+	baris();
+	srand(time(NULL));
+	int g = rand () % 21;
+	int h = rand () % 21;
+	int i = rand () % 21;
+	getch();
+	cout<< "\n| Poin    :"<< g<< " "<< h<< " "<< i<< " "<< "\n";
+	baris();		
+	cout<< "\n| Total : "<< g + h + i<< "\n";
+	baris();
 }
 
 	
